67305805ef9acef5edf6bf9f2769f6793cbea37a
Satriya JP Rapandaka <54264239+Micko57PR@users.noreply.github.com>
2023-06-05 16:51:45 +0700

    71170212_TemplateUGGraph.py

--- 71170212_TemplateUGGraph.py
+++ 71170212_TemplateUGGraph.py
@@ -10,12 +10,19 @@ class Graph:
 
     def vertex(self): #mencetak seluruh vertex
         print("\nSeluruh Node = ", end = "")
-        # tulis kode Anda di bawah sini
+        for key, value in self._data.items():
+            print(key, end = ' ')
         print()
     
     def edge(self): #mencetak seluruh edge
         print("Seluruh Edge = ", end = "")
-        # tulis kode Anda di bawah sini
+        listEdge = set()
+        for key, value in self._data.items():
+            for item in self._data[key]:
+               if key+item not in listEdge and item+key not in listEdge:
+                   listEdge.add(key+item)
+            for item in listEdge:
+                print(item, end = ' ')
         print()
 
     def addEdge(self, x, y):
@@ -25,25 +32,32 @@ class Graph:
             self._data[y].add(x) #hanya digunakan jika graph tidak berarah
 
     # untuk pembacaan traversing bfs graph
-    def bfs(self, node):
-        # silahkan tulis kode Anda di bawah ini 
-        print("\n")
+    # def bfs(self, node):
+    #     visited.append(node)
+    #     if node == y:
+    #         print(visited)
+    #     else:
+    #         for item in self._data[node]:
+    #             if item not in visited:
+    #                 self.dfs(item, y, visited)
+
+    #     print("\n")
 
 # silahkan buat graph seperti pada soal
 graph = Graph()
 # misalnya 
-# graph.addVertex('x')
-# graph.addVertex('y')
-# graph.addVertex('z')
-# graph.addVertex('j')
-# graph.addVertex('k')
+graph.addVertex('x')
+graph.addVertex('y')
+graph.addVertex('z')
+graph.addVertex('j')
+graph.addVertex('k')
 
-# graph.addEdge('x', 'y')
-# graph.addEdge('x','z')
-# graph.addEdge('y', 'z')
-# graph.addEdge('z', 'j')
+graph.addEdge('x', 'y')
+graph.addEdge('x','z')
+graph.addEdge('y', 'z')
+graph.addEdge('z', 'j')
 
 # jangan ubah bagian di bawah 
 graph.vertex()
 graph.edge()
-graph.bfs("a")
+# graph.bfs("a")
